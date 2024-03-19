f276655e1a1e7ac12060af9322d9dac2da467461
parsa <parsaes771385@gmail.com>
2024-03-19 21:10:57 +0330

    create shop left side pagination

--- src/components/ShopPage/LeftSide/LeftSide.jsx
+++ src/components/ShopPage/LeftSide/LeftSide.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import LeftSideHeader from '../LeftSideHeader/LeftSideHeader'
-import LeftSideColProductBox from '../LeftSideColProductBox/LeftSideColProductBox'
-import LeftSideRowProductBox from '../LeftSideRowProductBox/LeftSideRowProductBox'
+import React from "react";
+import LeftSideHeader from "../LeftSideHeader/LeftSideHeader";
+import LeftSideColProductBox from "../LeftSideColProductBox/LeftSideColProductBox";
+import LeftSideRowProductBox from "../LeftSideRowProductBox/LeftSideRowProductBox";
+import LeftSidePagination from "../LeftSidePagination/LeftSidePagination";
 
 function LeftSide() {
   return (
     <div className="flex-[4]">
       <LeftSideHeader />
 
-      <div className='grid grid-cols-1 gap-4 mt-4'>
+      <div className="grid grid-cols-1 gap-4 my-4">
         <LeftSideRowProductBox />
         <LeftSideRowProductBox />
         <LeftSideRowProductBox />
         <LeftSideRowProductBox />
         <LeftSideRowProductBox />
       </div>
+
+      <LeftSidePagination />
     </div>
-  )
+  );
 }
 
-export default LeftSide
+export default LeftSide;
---
+++ src/components/ShopPage/LeftSidePagination/LeftSidePagination.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+
+import "./LeftSidePagination.css"
+
+function LeftSidePagination() {
+  return (
+    <div className="border p-3 flex justify-center gap-2 [&>*]:transition [&>*]:duration-300 [&>*]:rounded [&>*:hover]:text-white [&>*:hover]:bg-primaryBlue">
+      <Link className="text-black bg-[#f0f0f0] flex items-center justify-center px-2">
+        <KeyboardDoubleArrowRightIcon fontSize="" />
+      </Link>
+      <Link className="text-black bg-[#f0f0f0] py-[3px] px-2.5 pagination-active">۱</Link>
+      <Link className="text-black bg-[#f0f0f0] py-[3px] px-2.5">۲</Link>
+      <Link className="text-black bg-[#f0f0f0] py-[3px] px-2.5">۳</Link>
+      <Link className="text-black bg-[#f0f0f0] flex items-center justify-center px-2">
+        <KeyboardDoubleArrowLeftIcon fontSize="" />
+      </Link>
+    </div>
+  );
+}
+
+export default LeftSidePagination;
