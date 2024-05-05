846c32e6ccb80eb1da3eaab129f5b02252351bb6
parsa <parsaes771385@gmail.com>
2024-05-05 14:21:42 +0430

    responsive contact page

--- src/components/ContactPage/ContactForm/ContactForm.jsx
+++ src/components/ContactPage/ContactForm/ContactForm.jsx
@@ -7,14 +7,14 @@ import { Link } from "react-router-dom";
 function ContactForm() {
   return (
     <section className="px-4 xs:px-24 mb-16">
-      <div className="flex flex-col md:flex-row items-start gap-x-8 gap-y-16">
+      <div className="flex flex-col lg:flex-row items-start gap-x-8 gap-y-16">
         <div className="flex-1">
           <h1 className="text-2xl font-IRANSans tracking-tighter">
             تماس با ما
           </h1>
           <div className="mt-4">
             <div className="border-b py-4">
-              <p className="text-justify text-[17px] leading-7">
+              <p className="text-justify text-sm lg:text-[17px] leading-6 lg:leading-7">
                 لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
                 استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
                 در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد
@@ -26,7 +26,7 @@ function ContactForm() {
               <span>
                 <LocationOnIcon fontSize="small" />
               </span>
-              <p>
+              <p className="text-sm lg:text-base">
                 تبریز، چهارراه آبرسان، فلکه دانشگاه، برج بلور، طبقه ۶۷۸، واحد ۸۵
               </p>
             </div>
@@ -34,40 +34,40 @@ function ContactForm() {
               <span>
                 <LocalPhoneIcon fontSize="small" />
               </span>
-              <p>۷۸۹ ۴۵۶ ۱۲۳(۹۸)+ , ۷۸۹ ۴۵۶ ۱۲۳(۹۸)+</p>
+              <p className="text-sm lg:text-base">۷۸۹ ۴۵۶ ۱۲۳(۹۸)+ , ۷۸۹ ۴۵۶ ۱۲۳(۹۸)+</p>
             </div>
             <div className="py-4 flex gap-4">
               <span>
                 <MailOutlineIcon fontSize="small" />
               </span>
-              <Link className="hover:text-primaryBlue transition duration-300">
+              <Link className="hover:text-primaryBlue text-sm lg:text-base transition duration-300">
                 info@sample.com
               </Link>
             </div>
           </div>
         </div>
-        <div className="flex-1">
+        <div className="flex-1 w-full">
           <h1 className="text-2xl font-IRANSans tracking-tighter">
             با ما در میان بگذارید
           </h1>
           <form className="mt-8 space-y-7">
             <div>
-              <label htmlFor="name" className="text-[17px]">نام شما (الزامی)</label>
-              <input type="text" id="name" placeholder="نام" className="w-full rounded-sm mt-2 outline-none border block py-2 px-6" />
+              <label htmlFor="name" className="lg:text-[17px]">نام شما (الزامی)</label>
+              <input type="text" id="name" placeholder="نام" className="w-full rounded-sm text-sm lg:text-base mt-2 outline-none border block py-2 px-6" />
             </div>
             <div>
-              <label htmlFor="email" className="text-[17px]">ایمیل شما (الزامی)</label>
-              <input type="text" id="email" placeholder="ایمیل" className="w-full rounded-sm mt-2 outline-none border block py-2 px-6" />
+              <label htmlFor="email" className="lg:text-[17px]">ایمیل شما (الزامی)</label>
+              <input type="text" id="email" placeholder="ایمیل" className="w-full rounded-sm text-sm lg:text-base mt-2 outline-none border block py-2 px-6" />
             </div>
             <div>
-              <label htmlFor="category" className="text-[17px]">موضوع</label>
-              <input type="text" id="category" placeholder="موضوع" className="w-full rounded-sm mt-2 outline-none border block py-2 px-6" />
+              <label htmlFor="category" className="lg:text-[17px]">موضوع</label>
+              <input type="text" id="category" placeholder="موضوع" className="w-full rounded-sm text-sm lg:text-base mt-2 outline-none border block py-2 px-6" />
             </div>
             <div>
-              <label htmlFor="category" className="text-[17px]">پیام شما</label>
-              <textarea type="text" rows="8" id="category" placeholder="پیام" className="w-full rounded-sm mt-2 outline-none border block py-2 px-6 resize-none" />
+              <label htmlFor="category" className="lg:text-[17px]">پیام شما</label>
+              <textarea type="text" rows="8" id="category" placeholder="پیام" className="w-full rounded-sm text-sm lg:text-base mt-2 outline-none border block py-2 px-6 resize-none" />
             </div>
-            <button className="text-white bg-zinc-800 text-lg py-2 px-7 rounded hover:bg-primaryBlue transition duration-300">ارسال</button>
+            <button className="text-white bg-zinc-800 lg:text-lg py-2 px-7 rounded hover:bg-primaryBlue transition duration-300">ارسال</button>
           </form>
         </div>
       </div>
