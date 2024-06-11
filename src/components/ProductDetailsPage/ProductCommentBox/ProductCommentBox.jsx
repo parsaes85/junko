f193e489e950827b349553b49965034b6ee780cb
parsa <parsaes771385@gmail.com>
2024-06-11 15:29:44 +0430

    show product comment answers

--- src/components/ProductDetailsPage/ProductCommentBox/ProductCommentBox.jsx
+++ src/components/ProductDetailsPage/ProductCommentBox/ProductCommentBox.jsx
@@ -6,31 +6,62 @@ import StarBorderIcon from "@mui/icons-material/StarBorder";
 function ProductCommentBox(props) {
   console.log(props);
   return (
-    <div className="flex gap-4">
-      <div>
-        <img src="/images/comment2.jpg" alt="" />
-      </div>
-      <div className="w-full border p-3 xs:p-4 space-y-4 relative after:absolute after:w-2.5 after:h-2.5 after:bg-white after:border-t after:border-r after:-right-1.5 after:top-2 after:rotate-45">
-        <div className="flex items-center justify-between">
-          <div className="tracking-tighter">
-            {props.user.username} - ۱۲ آذر ۱۳۹۸
-          </div>
-          <div className="text-primaryBlue text-sm flex">
-            {props.score &&
-              Array(5 - props.score)
+    <div>
+      <div className="flex gap-4">
+        <div>
+          <img src="/images/comment2.jpg" alt="" />
+        </div>
+        <div className="w-full border p-3 xs:p-4 space-y-4 relative after:absolute after:w-2.5 after:h-2.5 after:bg-white after:border-t after:border-r after:-right-1.5 after:top-2 after:rotate-45">
+          <div className="flex items-center justify-between">
+            <div className="tracking-tighter flex gap-1.5">
+              <span>{props.user.username}</span> <span>- ۱۸ آذر ۱۴۰۲</span>
+            </div>
+            <div className="text-primaryBlue text-sm flex">
+              {props.score &&
+                Array(5 - props.score)
+                  .fill(1)
+                  .map((start, index) => (
+                    <StarBorderIcon key={index} fontSize="" />
+                  ))}
+              {Array(props.score)
                 .fill(1)
                 .map((start, index) => (
-                  <StarBorderIcon key={index} fontSize="" />
+                  <StarIcon key={index} fontSize="" />
                 ))}
-            {Array(props.score)
-              .fill(1)
-              .map((start, index) => (
-                <StarIcon key={index} fontSize="" />
-              ))}
+            </div>
           </div>
+          <p className="text-[15px]">{props.body}</p>
         </div>
-        <p className="text-[15px]">{props.body}</p>
       </div>
+      {props.answers.length &&
+        props.answers.map((answer) => (
+          <div className="flex gap-4 mt-2 mr-10">
+            <div>
+              <img src="/images/comment2.jpg" alt="" />
+            </div>
+            <div className="w-full border p-3 xs:p-4 space-y-4 relative after:absolute after:w-2.5 after:h-2.5 after:bg-white after:border-t after:border-r after:-right-1.5 after:top-2 after:rotate-45">
+              <div className="flex items-center justify-between">
+                <div className="tracking-tighter flex gap-1.5">
+                  <span>{answer.username}</span> <span>- ۱۸ آذر ۱۴۰۲</span>
+                </div>
+                <div className="text-primaryBlue text-sm flex">
+                  {answer.score &&
+                    Array(5 - answer.score)
+                      .fill(1)
+                      .map((start, index) => (
+                        <StarBorderIcon key={index} fontSize="" />
+                      ))}
+                  {Array(answer.score)
+                    .fill(1)
+                    .map((start, index) => (
+                      <StarIcon key={index} fontSize="" />
+                    ))}
+                </div>
+              </div>
+              <p className="text-[15px]">{answer.body}</p>
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
