4d076f4f9bfe4c640faa944e7e63758563959dbe
parsa <parsaes771385@gmail.com>
2024-07-24 15:03:57 +0430

    show blog comment answers in blogDetails page

--- src/components/BlogDetailsPage/RightSideCommentBox/RightSideCommentBox.jsx
+++ src/components/BlogDetailsPage/RightSideCommentBox/RightSideCommentBox.jsx
@@ -2,24 +2,49 @@ import React from "react";
 
 function RightSideCommentBox(props) {
   return (
-    <div className="flex gap-4">
-      <div>
-        <img src="/images/comment2.jpg" alt="" />
-      </div>
-      <div className="border w-full p-3 xs:p-4 space-y-2">
-        <div className="flex items-start justify-between">
-          <div className="space-y-2">
-            <h3 className="text-[15px]">{props.user.username}</h3>
-            <p className="text-gray-500 tracking-tighter text-[13px]">
-              ۱۶ آبان ۱۴۰۲ ساعت ۱:۳۸ صبح
-            </p>
-          </div>
-          <button className="bg-zinc-800 text-white px-3 py-0.5 rounded text-[15px]">
-            پاسخ
-          </button>
+    <div>
+      <div className="flex gap-4">
+        <div>
+          <img src="/images/comment2.jpg" alt="" />
         </div>
-        <p>{props.body}</p>
+        <div className="border w-full p-3 xs:p-4 space-y-2">
+          <div className="flex items-start justify-between">
+            <div className="space-y-2">
+              <h3 className="text-[15px]">{props.user.username}</h3>
+              <p className="text-gray-500 tracking-tighter text-[13px]">
+                ۱۶ آبان ۱۴۰۲ ساعت ۱:۳۸ صبح
+              </p>
+            </div>
+            <button className="bg-zinc-800 text-white px-3 py-0.5 rounded text-[15px]">
+              پاسخ
+            </button>
+          </div>
+          <p>{props.body}</p>
+        </div>
       </div>
+      {props.answers.length
+        ? props.answers.map((answer) => (
+            <div className="flex gap-4 w-[95%] mr-auto mt-6">
+              <div>
+                <img src="/images/comment2.jpg" alt="" />
+              </div>
+              <div className="border w-full p-3 xs:p-4 space-y-2">
+                <div className="flex items-start justify-between">
+                  <div className="space-y-2">
+                    <h3 className="text-[15px]">{answer.username}</h3>
+                    <p className="text-gray-500 tracking-tighter text-[13px]">
+                      ۱۶ آبان ۱۴۰۲ ساعت ۱:۳۸ صبح
+                    </p>
+                  </div>
+                  <button className="bg-zinc-800 text-white px-3 py-0.5 rounded text-[15px]">
+                    پاسخ
+                  </button>
+                </div>
+                <p>{answer.body}</p>
+              </div>
+            </div>
+          ))
+        : ""}
     </div>
   );
 }
